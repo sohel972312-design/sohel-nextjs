ad2a64a1a4d8c3092d063d8852380ac54891cc37
sohel972312-design <sohel972312@gmail.com>
2026-03-14 21:17:09 +0530

    sdf

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function Contact() {
     return(
 <>
-Contact page
+Contact page new contact sam
 link to <Link className="text-white" href="/about">About</Link>
 link to <Link className="text-white" href="/">Home</Link>
 
